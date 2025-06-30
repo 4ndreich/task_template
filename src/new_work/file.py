3da99480046aee35943f390359fc3f15b5489c08
4ndreich <nik.art.98@mail.ru>
2025-06-30 21:15:24 +0300

    fix обратный цикл

--- src/new_work/file.py
+++
@@ -1,4 +0,0 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print (my_list)
-reversed_my_list = my_list[::-1]
-print(reversed_my_list)
--- src/order/file.py
+++ src/order/file.py
@@ -1,6 +1,10 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print (my_list)
-reversed_my_list = my_list[::-1]
-print(reversed_my_list)
+print(my_list)
+reversed_my_list = []
+index = len(my_list) - 9
+while index >= 0:
+    list = my_list[index]
+    reversed_my_list.append(list)
+    index -= 1
 
-
+print(reversed_my_list)   
