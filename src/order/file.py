77af9cdc7ea61ea10b1a3116c17731c79031b1a4
4ndreich <nik.art.98@mail.ru>
2025-06-30 22:28:38 +0300

    Сумма чисел из массива

--- src/order/file.py
+++ src/order/file.py
@@ -1,10 +1,5 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(my_list)
-reversed_my_list = []
-index = len(my_list) - 9
-while index >= 0:
-    list = my_list[index]
-    reversed_my_list.append(list)
-    index -= 1
-
-print(reversed_my_list)   
+my_list = [1, 2, 3, 4, 5]
+print ("Введите число:")
+choice = int(input())
+result = sum(my_list, choice)
+print(f"Сумма списка {my_list} и введенного числа {choice} равна: {result}")
