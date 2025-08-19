b95da5be93cb6987237b0b499a0477b0bc9dcd64
4ndreich <nik.art.98@mail.ru>
2025-08-19 20:52:28 +0300

    палиндром

---
+++ src/functions/word.py
@@ -0,0 +1,8 @@
+def is_palindrome(word):
+    processed_word = ''.join(filter(str.isalnum, word)).lower()
+    return processed_word == processed_word[::-1]
+user_input = input("Введите слово: ")
+if is_palindrome(user_input):
+    print(f'"{user_input}" - палиндром')
+else:
+    print(f'"{user_input}" - не палиндром')
