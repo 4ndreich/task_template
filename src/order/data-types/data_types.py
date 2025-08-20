a647f5b46ff5840c9e5a7bbe65f88e4b0d298ece
4ndreich <nik.art.98@mail.ru>
2025-08-20 15:22:01 +0300

    мои данные

---
+++ src/order/data-types/data_types.py
@@ -0,0 +1,10 @@
+def i ():
+    name =str("Артём")
+    age =int(26)
+    country = { "Россия" :
+    "Новосибирск, "
+    "Кировский район"}
+    formatted_string = ', '.join(f'{key}: {value}' for key, value in country.items())
+    profession = str("polismen")
+    print(f"\nВаше имя:'{name}',\nВаш возраст:'{age}',\nСтрана проживания:'{formatted_string}',\nВаша проффесия:'{profession}'")
+i()
