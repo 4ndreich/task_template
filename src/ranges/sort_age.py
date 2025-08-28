742982c1a7641041f4beadc2005278d9f2a6497e
4ndreich <nik.art.98@mail.ru>
2025-08-28 22:52:03 +0300

    fix сортировка возраста

--- src/ranges/sort_age.py
+++ src/ranges/sort_age.py
@@ -1,3 +1,11 @@
+MAX_KIDS_AGE = 12
+MAX_TEENAGERS_AGE = 18
+MAX_CLUB27_AGE = 27
+MAX_YOUNG_AGE = 45
+MAX_MATURE_AGE = 60
+MAX_ELDERLY_AGE = 74
+MAX_YOUNG_AT_HEART = 90
+
 print("Введите ваш возраст")
 while True:
     age = input()
@@ -6,19 +14,19 @@ while True:
     else:
         age = int(age)
         break
-if age <= 12:
+if age <= MAX_KIDS_AGE:
     print("От 0 до 12 - дети")
-elif age <= 18:
+elif age <= MAX_TEENAGERS_AGE:
     print("От 12 до 18 - подростки")
-elif age <= 27:
+elif age <= MAX_CLUB27_AGE:
     print("От 18 до 27 - клуб 27")
-elif age <= 45:
+elif age <= MAX_YOUNG_AGE:
     print("От 27 до 45 - молодые")
-elif age <= 60:
+elif age <= MAX_MATURE_AGE:
     print("От 45 до 60 - зрелые")
-elif age <= 74:
+elif age <= MAX_ELDERLY_AGE:
     print("От 60 до 74 - пожилые")
-elif age <= 90:
+elif age <= MAX_YOUNG_AT_HEART:
     print("От 74 до 90 - молоды в душе")
 else:
     print("От 90 и выше - долгожители")
