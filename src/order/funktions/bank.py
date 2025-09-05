1e6491624cbc1b5723a4fd37ef13ec30f80e4039
4ndreich <nik.art.98@mail.ru>
2025-09-05 22:37:52 +0300

    fix банк

--- src/order/funktions/bank.py
+++ src/order/funktions/bank.py
@@ -5,18 +5,22 @@ def bank(n, years):
 def main():
     try:
         money = float(input('Сколько у вас денег для вклада? (рубли): '))
-
-        years = int(input('На какой срок хотите сделать вклад? (лет): '))
-        if money <= 0 or years <= 0:
-            print('Ошибка: сумма и срок должны быть положительными числами.')
-            return
-
-        result = bank(money, years)
-
-        print(f'Через {years} лет на вашем счету будет: {result:.2f} рублей')
-
     except ValueError:
-        print('Ошибка: пожалуйста, вводите числовые значения.')
+        print('Ошибка: пожалуйста, вводите числовые значения для суммы.')
+        return
+
+    try:
+        years = int(input('На какой срок хотите сделать вклад? (лет): '))
+    except ValueError:
+        print('Ошибка: пожалуйста, вводите целое число для срока.')
+        return
+
+    if money <= 0 or years <= 0:
+        print('Ошибка: сумма и срок должны быть положительными числами.')
+        return
+
+    result = bank(money, years)
+    print(f'Через {years} лет на вашем счету будет: {result:.2f} рублей')
 
 
 if __name__ == '__main__':
